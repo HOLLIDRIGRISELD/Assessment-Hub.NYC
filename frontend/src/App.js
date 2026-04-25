4a34b6ca8d8445c892021ad23edbd50615fb6751
HOLLIDRIGRISELD <hollidrigriseldi@gmail.com>
2026-04-25 23:31:21 +0300

    created the fontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Atlas Financial Prototype</h1>
+      <p>React Frontend is connected!</p>
+    </div>
+  );
+}
+
+export default App;
